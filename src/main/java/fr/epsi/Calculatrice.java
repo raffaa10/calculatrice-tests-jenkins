ae24f30a43fc6a8e5fbbfea5f308c0ceec4bd896
raazi <rafayel.azizyan@gmail.com>
2022-02-15 10:09:37 +0100

    test calculator via Jenkins

---
+++ src/main/java/fr/epsi/Calculatrice.java
@@ -0,0 +1,49 @@
+package fr.epsi;
+
+public class Calculatrice {
+
+    /**
+     * method addition
+     * @param a
+     * @param b
+     * @return a+b
+     */
+    public double addition(double a, double b) {
+        return a + b;
+    }
+
+    /**
+     *
+     * @param a
+     * @param b
+     * @return a-b
+     */
+    public double soustraction(double a, double b) {
+        return a -b;
+    }
+
+    /**
+     *
+     * @param a
+     * @param b
+     * @return a * b
+     */
+    public double multiplication(double a, double b) {
+        return a * b;
+    }
+
+    /**
+     *
+     * @param a
+     * @param b
+     * @return a / b
+     */
+    public double division(double a, double b) {
+      /*  if(b == 0) {
+            throw new ArithmeticException("Division by 0");
+        } else {
+            return a / b;
+        }*/
+        return a / b;
+    }
+}
---
+++ src/test/java/CalculatriceTest.java
@@ -0,0 +1,62 @@
+import fr.epsi.Calculatrice;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Test Calculatrice class")
+public class CalculatriceTest {
+
+    private Calculatrice calculatrice;
+
+    @BeforeEach
+    public void initTest() {
+        calculatrice = new Calculatrice();
+    }
+
+    @Test
+    @DisplayName("10 plus 5 test")
+    public void testAddition() {
+        double a = 10;
+        double b = 5;
+        assertEquals(15, calculatrice.addition(a, b));
+    }
+
+    @Test
+    @DisplayName("4 moins 1 test")
+    public void testSoustraction() {
+        double a = 4;
+        double b = 1;
+        assertEquals(3, calculatrice.soustraction(a, b));
+    }
+
+    @Test
+    @DisplayName("Multiply 3 by 2 ")
+    public void testMultiplication() {
+        double a = 2;
+        double b = 3;
+        assertEquals(6, calculatrice.multiplication(a, b));
+    }
+
+    @Test()
+    @DisplayName("Division by zero")
+    public void testDivision() {
+        double a = 7;
+        double b = 0;
+        try {
+            calculatrice.division(a, b);
+        } catch (ArithmeticException e) {
+            throw new ArithmeticException("Division by 0");
+        }
+    }
+
+    @Test
+    @DisplayName(" 9 divided by 3 ")
+    public void testMultiplication2() {
+        double a = 9;
+        double b = 3;
+        assertEquals(3, calculatrice.division(a, b));
+    }
+
+}
